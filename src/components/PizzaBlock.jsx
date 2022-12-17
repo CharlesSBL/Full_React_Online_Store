7d31d9ff78afa709663bc0e54db1e37a3e214447
CharlesSBL <77samaritan77@proton.me>
2022-12-17 23:02:04 +0100

    UP_1_1_4

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -6,6 +6,7 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
 
   return (
     <div className="pizza-block">
+      <div></div>
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
